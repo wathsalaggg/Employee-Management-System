6192da41a5ce076052b61f1130f9e6472a3fd4f1
Wathsala Gamarachchi <72685782+wathsalaggg@users.noreply.github.com>
2022-12-26 01:16:21 +0530

    mapping

--- spring boot backend/src/main/java/controller/EmployeeController.java
+++ spring boot backend/src/main/java/controller/EmployeeController.java
@@ -1,8 +1,27 @@
 package controller;
 
+import model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import repository.EmployeeRepository;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/")
 public class EmployeeController {
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+
+        return employeeRepository.findAll();
+
+
+    }
 }
