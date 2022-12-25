c23a25533b3e6983c168ebd044621313fe57ea1f
Wathsala Gamarachchi <72685782+wathsalaggg@users.noreply.github.com>
2022-12-25 22:20:31 +0530

    more java

--- spring boot backend/src/main/java/model/Employee.java
+++ spring boot backend/src/main/java/model/Employee.java
@@ -1,4 +1,55 @@
 package model;
 
+//import javax.persistence.Entity;
+
+
 public class Employee {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+
+    public Employee() {
+
+    }
+
+    public Employee(String firstName, String lastName, String emailId) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
 }
