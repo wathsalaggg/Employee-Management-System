f51ec0cbbabd20c74e3c5d74adfdac4e8b613720
Wathsala Gamarachchi <72685782+wathsalaggg@users.noreply.github.com>
2022-12-25 23:39:56 +0530

    added interfaces

--- spring boot backend/src/main/java/model/Employee.java
+++ spring boot backend/src/main/java/model/Employee.java
@@ -2,11 +2,26 @@ package model;
 
 //import javax.persistence.Entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+
 
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private long id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "email_id")
     private String emailId;
 
     public Employee() {
