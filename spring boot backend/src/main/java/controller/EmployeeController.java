5d1c9d575315aa18dedcc04c05010cb197bd545d
Wathsala Gamarachchi <72685782+wathsalaggg@users.noreply.github.com>
2022-12-31 02:04:21 +0530

    connection successful for now. but still jdbc is now working

--- spring boot backend/src/main/java/controller/EmployeeController.java
+++ spring boot backend/src/main/java/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package controller;
 
 import model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import repository.EmployeeRepository;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
