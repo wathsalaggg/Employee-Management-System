2f9f98814cfc64d2bb9fb01c48364086007e2e91
Wathsala Gamarachchi <72685782+wathsalaggg@users.noreply.github.com>
2022-12-31 23:07:45 +0530

    configured routing

--- react frontend/fe/src/App.js
+++ react frontend/fe/src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router , Route, Switch} from 'react-router-dom'
+
+
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
@@ -7,17 +10,36 @@ import FooterComponent from './components/FooterComponent';
 function App() {
   return (
     
-    <><><div>
+    <div>
+
+      <Router> 
+        <div className='container'> 
+
+<switch> http://localhost:3000/employees
+
+  <Route path = "/"  component = {ListEmployeeComponent}>  </Route>
+  <Route path = "/employees"  component = {ListEmployeeComponent}>  </Route>
+
+
+   <ListEmployeeComponent/> 
+</switch>
+
       <HeaderComponent />
 
     </div><div className="container">
 
         <ListEmployeeComponent />
 
-      </div></><div>
+      </div>
+      <div>
         <FooterComponent />
-      </div></>
+</div>
+
+        </Router> 
+      </div>
   );
+
 }
+
 
 export default App;
