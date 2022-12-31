2ffda0e9087ac0c291626c49fee95e1896f79a27
Wathsala Gamarachchi <72685782+wathsalaggg@users.noreply.github.com>
2023-01-01 01:57:53 +0530

    add employee done

--- react frontend/fe/src/App.js
+++ react frontend/fe/src/App.js
@@ -6,6 +6,7 @@ import {BrowserRouter as Router , Route, Switch} from 'react-router-dom'
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
+import CreateEmployeeComponent from './components/CreateEmployeeComponent';
 
 function App() {
   return (
@@ -20,13 +21,14 @@ function App() {
 
         <div className='container'> 
 
-<switch> http://localhost:3000/employees
+<switch> http://localhost:3000/add-employee
 
   <Route path = "/"  component = {ListEmployeeComponent}>  </Route>
   <Route path = "/employees"  component = {ListEmployeeComponent}>  </Route>
+  <Route path = "/add-employee"  component = {CreateEmployeeComponent}>  </Route>
 
 
-   <ListEmployeeComponent/> 
+   
 </switch>
 
       
